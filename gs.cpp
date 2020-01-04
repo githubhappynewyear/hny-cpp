66d28563a99ad1b22f53ad2823a7cc73bcde7acf
githubhappynewyear <46952765+githubhappynewyear@users.noreply.github.com>
2020-01-04 17:00:39 +0800

    Add files via upload

---
+++ gs.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	
+    return 0;
+}
+/*
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<string>
+#include<cmath>
+#include<cctype>
+#include<iomanip>
+#include<algorithm>
+#include<queue>
+#include<map>
+using namespace std;
+
+*/ 
